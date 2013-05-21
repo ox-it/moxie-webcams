01b0e402142e89d05759f29b715745e0f75643d8
Martin Filliau <martin@filliau.com>
2013-05-21 10:53:20 +0100

    Added initial setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+install_requires = open('requirements.txt').readlines()
+
+setup(name='moxie-webcams',
+    version='0.1',
+    packages=find_packages(),
+    description='Webcams module for Moxie',
+    author='Mobile Oxford',
+    author_email='mobileoxford@it.ox.ac.uk',
+    url='https://github.com/ox-it/moxie-webcams',
+    include_package_data=True,
+    setup_requires=["setuptools"],
+    install_requires=install_requires,
+    test_suite="moxie_webcams.tests",
+)
